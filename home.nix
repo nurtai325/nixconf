d0d1530f506de65e5ab4d4d39761db5095d0eca2
nurtai325 <nurtolymbek23@gmail.com>
2026-03-15 01:22:22 +0500

    chore: refactoring

--- home.nix
+++ home.nix
@@ -71,6 +71,29 @@ in
       IndentWidth: 4
       TabWidth: 4
     '';
+    ".config/clangd/config.yaml".text =
+      let
+        clang = pkgs.clang.cc;
+        clangVersion = pkgs.clang.cc.version;
+      in
+      ''
+        CompileFlags:
+          Add:
+            - "-isystem"
+            - "${clang}/include/c++/${clangVersion}"
+            - "-isystem"
+            - "${clang}/include/c++/${clangVersion}/x86_64-unknown-linux-gnu"
+            - "-isystem"
+            - "${clang}/include/c++/${clangVersion}/backward"
+            - "-isystem"
+            - "${clang}/lib/gcc/x86_64-unknown-linux-gnu/${clangVersion}/include"
+            - "-isystem"
+            - "${clang}/include"
+            - "-isystem"
+            - "${clang}/lib/gcc/x86_64-unknown-linux-gnu/${clangVersion}/include-fixed"
+            - "-isystem"
+            - "${pkgs.glibc.dev}/include"
+      '';
   };
 
   home.sessionVariables = {
