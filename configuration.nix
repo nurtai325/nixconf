742a0f337742210f69c1de5ed1a18546bcd55e80
nurtai325 <nurtolymbek23@gmail.com>
2026-04-14 15:13:43 +0500

    chore: refactoring

--- configuration.nix
+++ configuration.nix
@@ -10,7 +10,7 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages;
+  boot.kernelPackages = pkgs.linuxPackages_zen;
   boot.initrd.luks.devices."cryptroot".device = "/dev/disk/by-label/NIXOS_LUKS";
   system.nixos.label = "Personal";
 
